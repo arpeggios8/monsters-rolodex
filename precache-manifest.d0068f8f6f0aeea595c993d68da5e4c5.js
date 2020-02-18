13429ed99a0d623bfbd076c961b791f7b51c6f8c
arpeggios8 <58250850+arpeggios8@users.noreply.github.com>
2020-02-18 11:10:17 +0800

    Updates

---
+++ precache-manifest.d0068f8f6f0aeea595c993d68da5e4c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a68a9fd69edcdd6e390b1eda7cc9ef6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "04e0f20b8954ae76e0ea",
+    "url": "/monsters-rolodex/static/css/main.9e5635fb.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e0f20b8954ae76e0ea",
+    "url": "/monsters-rolodex/static/js/main.9e0e5eeb.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2db4a9dc8acae6c768b8f53ccd31071e.js"
+  "/monsters-rolodex/precache-manifest.d0068f8f6f0aeea595c993d68da5e4c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
